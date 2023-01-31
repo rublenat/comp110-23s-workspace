3510944284e402243d4e51caf53533743535a33e
Natalie Ruble <raqueln@ad.unc.edu>
2023-01-31 15:07:13 -0500

    While Loops notes nad EX02

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,40 @@
+"""EX02 - One Shot Wordle."""
+__author__ : "730396580"
+
+secret_word: str = "python"
+length_of_secret_word: int = len(secret_word)
+guess_word: str = input(f"What is your {length_of_secret_word}-letter guess? ")
+
+while len(guess_word) != length_of_secret_word:
+    guess_word: str = input(f"That was not {length_of_secret_word} letters! Try again: ")
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+idx: int = 0
+resulting_emoji: str = ""
+
+while idx < length_of_secret_word:
+    if guess_word[idx] == secret_word[idx]:
+        resulting_emoji = resulting_emoji + GREEN_BOX
+    else:
+        alternate_letter: bool = False
+        alternate_idx: int = 0
+
+        while alternate_letter != True and alternate_idx < length_of_secret_word:
+            if secret_word[alternate_idx] == guess_word[idx]:
+                alternate_letter = True
+            else:
+                alternate_idx = alternate_idx + 1
+        if alternate_letter == True:
+            resulting_emoji = resulting_emoji + YELLOW_BOX
+        else:
+            resulting_emoji = resulting_emoji + WHITE_BOX
+    idx = idx + 1
+print(resulting_emoji)
+
+if guess_word != secret_word:
+    print("Not quite. Play again soon!")
+else:
+    print("Woo! You got it!")
---
+++ lessons/while_loops.py
@@ -0,0 +1,16 @@
+"""demonstrates while loops by findng low value in string"""
+
+cards = str = "5235"
+
+card_idx: int = 0
+low_card: int = int(cards[0])
+
+# look at each number in a string
+while card_idx < 4:
+    # Check if current card is less than low card
+    current_card: int = int(cards[card_idx])
+    if current_card < low_card:
+        # update low card to value of current card
+        low_card = current_card
+    card_idx = card_idx + 1
+print(low_card)
