b2726feb2fbf6713b552bfe20655cb9e0caf2e80
Natalie Ruble <raqueln@ad.unc.edu>
2023-02-01 15:52:12 -0500

    EX02 with comments

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,10 +1,12 @@
 """EX02 - One Shot Wordle."""
 __author__ = "730396580"
 
+#Asking user for their guess on the secret word 
 secret_word: str = "python"
 length_of_secret_word: int = len(secret_word)
 guess_word: str = input(f"What is your {length_of_secret_word}-letter guess? ")
 
+#If the user's guess does not match the number of letters, gives an error and asks for input of another guess
 while len(guess_word) != length_of_secret_word:
     guess_word = input(f"That was not {length_of_secret_word} letters! Try again: ")
 
@@ -14,6 +16,10 @@ YELLOW_BOX: str = "\U0001F7E8"
 
 idx: int = 0
 resulting_emoji: str = ""
+
+#Checking to see if each index of secret word and guess word match, gives different color box based on outcome
+#Only repeating the loop while the index value is less than the length of the secret word
+#Index goes up by one after each loop in order for the whole word to be searched
 
 while idx < length_of_secret_word:
     if guess_word[idx] == secret_word[idx]:
@@ -34,6 +40,7 @@ while idx < length_of_secret_word:
     idx = idx + 1
 print(resulting_emoji)
 
+#If the guess word is the secret word tells the user they were right, if the guess was wrong tells them to guess again
 if guess_word != secret_word:
     print("Not quite. Play again soon!")
 else:
