3b6c9d3a0ccae7165cef08fc5e09decc00900879
Natalie Ruble <raqueln@ad.unc.edu>
2023-02-18 08:54:27 -0500

    EX03 autograder changes

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,12 +1,13 @@
-"""EX03 - Structured Wordle"""
-__author__ : "730396580"
+"""EX03 - Structured Wordle."""
+__author__ = "730396580"
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-def contains_char(word : str, character: str) -> bool:
-    """Searches for given character in given word"""
+
+def contains_char(word: str, character: str) -> bool:
+    """Searches for given character in given word."""
     assert len(character) == 1
     idx: int = 0
     in_word: bool = False
@@ -18,8 +19,9 @@ def contains_char(word : str, character: str) -> bool:
         idx = idx + 1
     return in_word
 
+
 def emojified(guess: str, secret_word: str) -> str:
-    """Returns emoji boxes based on whether characters in guess and secret word match"""
+    """Returns emoji boxes based on whether characters in guess and secret word match."""
     assert len(guess) == len(secret_word)
     idx: int = 0
     emoji: str = ""
@@ -28,15 +30,16 @@ def emojified(guess: str, secret_word: str) -> str:
         if guess[idx] == secret_word[idx]:
             emoji = emoji + GREEN_BOX
         else:
-            if contains_char(secret_word, guess[idx]) == True:
+            if contains_char(secret_word, guess[idx]) is True:
                 emoji = emoji + YELLOW_BOX
             else:
                 emoji = emoji + WHITE_BOX
         idx = idx + 1
     return emoji
 
+
 def input_guess(expected_length: int) -> str:
-    """Given expected length will ask user for guess word of that same length"""
+    """Given expected length will ask user for guess word of that same length."""
     word_given: str = input(f"Enter a {expected_length} character word: ")
 
     while len(word_given) != expected_length:
@@ -46,7 +49,6 @@ def input_guess(expected_length: int) -> str:
 
 def main() -> None:
     """The entry point of the program and the main game loop."""
-
     secret: str = "codes"
     turns: int = 1
     guess_word: str = ""
@@ -58,22 +60,12 @@ def main() -> None:
 
         if guess_word != secret:
             turns = turns + 1
-        
 
     if guess_word == secret:
         print(f"You won in {turns}/6 guesses!")
     if guess_word != secret:
         print("X/6 - Sorry, try again tomorrow!")
 
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
