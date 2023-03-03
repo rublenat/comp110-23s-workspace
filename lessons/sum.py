38a8330eec8382177ebd25a427231f83c3809f35
Natalie Ruble <raqueln@ad.unc.edu>
2023-03-03 11:16:30 -0500

    CQ3 ver 2

--- lessons/sum.py
+++ lessons/sum.py
@@ -27,7 +27,7 @@ def sum(xs: list[float]) -> float:
     """Return sum of all elements in xs."""
     sum_total: float = 0.0
 
-    for item in range(0, len(xs)+1):
+    for item in range(0, len(xs)):
         sum_total += item
     
     return sum_total
