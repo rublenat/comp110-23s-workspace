20f5f7b0ec128999c7b1c8241f3c36881a85541e
Natalie Ruble <raqueln@ad.unc.edu>
2023-03-03 10:48:54 -0500

    challenge question 2 version 3

--- lessons/sum.py
+++ lessons/sum.py
@@ -12,13 +12,24 @@ def sum_old(xs: list[float]) -> float:
     return sum_total
 
 
-def sum(xs: list[float]) -> float:
+def sum_old_too(xs: list[float]) -> float:
     """Return sum of all elements in xs."""
     sum_total: float = 0.0
     idx: int = 0
 
     if len(xs) > 0:
         item: int = xs[idx]
+        for item in xs:
+            sum_total += item
+    
+    return sum_total
+
+def sum(xs: list[float]) -> float:
+    """Return sum of all elements in xs."""
+    sum_total: float = 0.0
+    idx: int = 0
+
+    if len(xs) > 0:
         for item in xs:
             sum_total += item
     
