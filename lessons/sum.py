17be0b7a0c935b726785b2432a77f06271b8252a
Natalie Ruble <raqueln@ad.unc.edu>
2023-03-03 10:40:37 -0500

    chellenge question ver 2

--- lessons/sum.py
+++ lessons/sum.py
@@ -16,9 +16,10 @@ def sum(xs: list[float]) -> float:
     """Return sum of all elements in xs."""
     sum_total: float = 0.0
     idx: int = 0
-    item: int = xs[idx]
 
-    for item in xs:
-        sum_total += item
+    if len(xs) > 0:
+        item: int = xs[idx]
+        for item in xs:
+            sum_total += item
     
     return sum_total
