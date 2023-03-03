f27802b7d96e8463f6c5f681778e195f79e3a67f
Natalie Ruble <raqueln@ad.unc.edu>
2023-03-03 10:49:10 -0500

    CQ2 ver 3

--- lessons/sum.py
+++ lessons/sum.py
@@ -12,18 +12,6 @@ def sum_old(xs: list[float]) -> float:
     return sum_total
 
 
-def sum_old_too(xs: list[float]) -> float:
-    """Return sum of all elements in xs."""
-    sum_total: float = 0.0
-    idx: int = 0
-
-    if len(xs) > 0:
-        item: int = xs[idx]
-        for item in xs:
-            sum_total += item
-    
-    return sum_total
-
 def sum(xs: list[float]) -> float:
     """Return sum of all elements in xs."""
     sum_total: float = 0.0
